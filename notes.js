3e208b1af6583257a4f0387bf754c80fd09a04df
James <jamesjeremyb@gmail.com>
2023-03-13 19:42:14 -0700

    ch2 and notes

---
+++ notes.js
@@ -0,0 +1,55 @@
+// ================== if statements ==================
+/*
+et num = Number(prompt("Pick a number"));
+if (num < 10) {
+    console.log("Small");
+} else if (num < 100) {
+    console.log("Medium");
+} else {
+    console.log("Large");
+}
+*/
+
+// ================== while loop ==================
+/*
+let number = 0;
+while (number <= 12) {
+    console.log(number);
+    number = number + 2;
+}
+*/
+
+
+// ================== do while loops ==================
+/* 
+let yourName;
+do {
+    yourName = prompt("Who are you?");
+} while (!yourName);
+console.log(yourName); 
+*/
+
+
+// ================== for loops ==================
+/* 
+for (let number = 0; number <= 12; number += 2) {
+  console.log(number);
+}
+*/
+
+// ================== switch statement ==================
+/* 
+switch (prompt("What is the weather like?")) {
+    case "rainy":
+        console.log("Remember to bring an umbrella.");
+        break;
+    case "sunny":
+        console.log("Dress lightly.");
+    case "cloudy":
+        console.log("Go outside.");
+        break;
+    default:
+        console.log("Unknown weather type!");
+        break;
+} 
+*/
