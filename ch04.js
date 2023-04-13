cc0f42e360beaabd44726d95638b94d243b085d5
James <jamesjeremyb@gmail.com>
2023-04-13 12:24:15 -0700

    chapter 4 complete

--- ch04.js
+++ ch04.js
@@ -71,6 +71,7 @@ function reverseArrayInPlace(array) {
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue); */
 
+//------------------------------------------ Lists -----------------------------------------
 /* 
 Write a function arrayToList that builds up a list structure like the one shown when given[1, 2, 3] as argument.
 
@@ -89,5 +90,102 @@ function arrayToList(array) {
     }
 }
 
-console.log(arrayToList([10, 20]));
+//console.log(JSON.stringify(arrayToList([10, 20, 30, 40])));
 // → {value: 10, rest: {value: 20, rest: null}}
+
+function listToArray(list) {
+    var arr1 = [];
+    for (let node = list; node; node = node.rest){    
+            arr1.push(node.value);
+    }
+    return arr1;
+}
+//console.log(listToArray(arrayToList([10, 20, 30])));  // → [10, 20, 30]
+/* alt solution
+
+function arrayToList([...arr], n = 0) {
+    return (n < arr.length) ? { value: arr[n], rest: arrayToList(arr, n + 1) } : null;
+}
+
+function listToArray({ ...list }, arr = []) {
+    arr.push(list.value);
+    return (list.rest != null) ? listToArray(list.rest, arr) : arr;
+} 
+function prepend(value, list) {
+  return {value, rest: list};
+}
+
+function nth(list, n) {
+  if (!list) return undefined;
+  else if (n == 0) return list.value;
+  else return nth(list.rest, n - 1);
+}
+*/
+
+function prepend(element, list, newList = {}) {
+    newList.value = element;
+    newList.rest = list;
+    return newList;
+}
+//console.log(JSON.stringify(prepend(10, prepend(20, null))));
+// → {value: 10, rest: {value: 20, rest: null}}
+
+function nth(list, position) {
+    let listArray = listToArray(list);
+    return listArray[position];
+}
+//console.log(JSON.stringify(nth(arrayToList([10, 20, 30]), 1)));
+// → 20
+
+//------------------------------------------Deep comparison-----------------------------------------
+/* Write a function deepEqual that takes two values and returns true only if they are the same value or are objects with the same properties, where the values of the properties are equal when compared with a recursive call to deepEqual. 
+
+To find out whether values should be compared directly (use the === operator for that) or have their properties compared, you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison. But you have to take one silly exception into account: because of a historical accident, typeof null also produces "object".
+
+The Object.keys function will be useful when you need to go over the properties of objects to compare them
+
+*/
+
+const getProps = function (obj) {
+    for (let value in obj) {
+        if (isObj(obj[value])) {
+            getProps(obj[value]);
+        } else {
+            console.log("obj[value] : " + obj[value]);
+            console.log("value : " + value);
+        }
+    }
+};
+
+
+function deepEqual(value1, value2) {
+    if (value1 === value2) {
+        return true;
+    }
+    if (typeof value1 != 'object' || typeof value2 != 'object' || value1 == null || value2 == null) {
+        return false;
+    }
+
+    let val1Keys = Object.keys(value1);
+    let val2Keys = Object.keys(value2);
+    if (val1Keys.length != val2Keys.length) {
+        return false;
+    }
+
+    for (let key of val1Keys) { //for..of loops over values
+        if (!val2Keys.includes(key) || !deepEqual(value1[key]), value2[key]) {
+            return false;
+            }
+        }
+    return true;
+};
+
+   
+    
+let obj = { here: { is: "an" }, object: 2 };
+//console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, { here: 1, object: 2 }));
+// → false
+//console.log(deepEqual(obj, { here: { is: "an" }, object: 2 }));
+// → true
